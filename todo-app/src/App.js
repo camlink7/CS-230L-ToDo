ac73ea5157aa6a43f03c84008219296a6e9ac402
Camden <camdenlink7@gmail.com>
2023-11-13 22:24:02 -0500

    added add, delete, style, and animations

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import {React, useRef, useState} from "react";
 
 function App() {
+
+  const taskInput = useRef(null);
+  const [tasks, setTasks] = useState([]);
+
+  const addTaskBtn = (e) => {
+    e.preventDefault();
+    if (taskInput.current.value.trim()){
+      setTasks([...tasks, taskInput.current.value]);
+    }
+    taskInput.current.value = "";
+  }
+
+  const removeTask = (index, e) => {
+    let target = e.currentTarget.parentElement;
+    target.classList.add("fade-out");
+    setTimeout(() => {
+      target.classList.remove("fade-out")
+      setTasks(tasks.filter((_, i) => {
+        if (i !== index){
+          return true;
+        }
+        else {
+          return false;
+        }
+      }));
+    }, 200);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="w-100 justify-content-center text-center mt-5">
+        <div className="d-flex justify-content-center">
+          <div className="d-inline">
+            <h1>ToDo List</h1>
+            <form onSubmit={addTaskBtn}>
+              <input type="text" ref={taskInput} placeholder="Enter task name" className="task-input"/>
+              <button type="submit" className="add-btn">Add</button>
+            </form>
+          </div>
+        </div>
+
+        <div className="d-flex justify-content-center">
+          <div className="d-inline">
+            { tasks.map((task, i) => {
+              return (
+                <div className={`task-card d-flex w-100 ${i % 2 !== 0 && "odd"} fade-in`}>
+                  <p className="w-100 m-0">{task}</p>
+                  <button onClick={(e) => {removeTask(i, e);}} 
+                  className={`delete-btn`}>X</button>
+                </div>
+              )
+            })
+            }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
